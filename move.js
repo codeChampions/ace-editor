e7d367ad79cf39b832f7310177ddc84049e28e0c
tken85 <terrykennair@Terrys-MBP.attlocal.net>
2015-12-07 22:50:40 -0500

    fixed movement

--- move.js
+++ move.js
@@ -3,23 +3,19 @@ $(document).ready(function(){
 
 
   var moveLeft = function(){
-    var leftMove = $('#char').position().left - 50;
-    $('#char').animate({left: leftMove}, {duration: 500});
+    $('#char').animate({left: "-=50"}, {duration: 500});
   };
 
   var moveRight = function(){
-    var rightMove = $('#char').position().left + 50;
-    $('#char').animate({left: rightMove}, {duration: 500});
+    $('#char').animate({left: "+=50"}, {duration: 500});
   };
 
   var moveUp = function(){
-    var upMove = $('#char').position().top - 50;
-    $('#char').animate({top: upMove}, {duration: 500});
+    $('#char').animate({top: "-=50"}, {duration: 500});
   };
 
   var moveDown = function(){
-    var downMove = $('#char').position().top + 50;
-    $('#char').animate({top: downMove}, {duration: 500});
+    $('#char').animate({top: "+=50"}, {duration: 500});
   };
 
 
